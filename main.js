2d4600a7554483d4afed7ef6bd4c9bd1c1ca8dfd
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-09-03 13:24:20 -0400

    Start game board

---
+++ main.js
@@ -0,0 +1,16 @@
+const gameBoard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push("x");
+        }
+    }
+
+    return {
+        board,
+    }
+})();
